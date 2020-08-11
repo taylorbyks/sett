5d3db1d61edde6771e3f77c069eb4f55f26ac87e
Taylor Schinvelski <taylorbyks@hotmail.com>
2020-08-10 23:33:03 -0300

    Mobile CSS Update

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -6,9 +6,9 @@ import './SocialLinks.css'
 function SocialLinks(props) {
   return (
     <div className="SocialLinks">
-      <a className="SocialLink" href="http://facebook/"><FiFacebook style={{ color: "var(--primary)" }} size={props.size}/></a>
-      <a className="SocialLink" href="http://instagram/"><FiInstagram style={{ color: "var(--primary)" }} size={props.size}/></a>
-      <a className="SocialLink" href="http://whatsapp/"><FiPhoneCall style={{ color: "var(--primary)" }} size={props.size}/></a>
+      <a className="SocialLink" href="https://www.facebook.com/sett.seg"><FiFacebook style={{ color: "var(--primary)" }} size={props.size}/></a>
+      <a className="SocialLink" href="https://www.instagram.com/sett.seg"><FiInstagram style={{ color: "var(--primary)" }} size={props.size}/></a>
+      <a className="SocialLink" href="https://api.whatsapp.com/send?phone=5545999256754&text="><FiPhoneCall style={{ color: "var(--primary)" }} size={props.size}/></a>
     </div>
   );
 }
