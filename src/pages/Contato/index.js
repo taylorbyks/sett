065b0c7d37a96b2e1ef24bf2039b071565549f2b
Taylor Schinvelski <taylorbyks@hotmail.com>
2020-08-11 00:14:23 -0300

    Update

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -13,13 +13,13 @@ function Contato() {
        <h2 className="Title">Contato</h2>
        <div className="SecundaryC">
        <div className="Box">
-          <h2>Whatsapp - Janaina: 45 99925-6754</h2>
+          <h2>Whatsapp - Janaina: <br><b>45 99925-6754</b></br></h2>
        </div>
        <div className="Box">
-          <h2>Email: sett.adm@hotmail.com</h2>
+          <h2>Email: <b>sett.adm@hotmail.com</b></h2>
        </div>
        <div className="Box">
-        <h2>Acompanhe a Sett nas Redes Socias</h2>
+        <h2>Acompanhe a Sett nas Redes Socias</h2><br></br>
         <SocialLinks size={ 30 } />
        </div>
        </div>
