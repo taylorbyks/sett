f634421388ea469f05589459b5435617e2e87c70
Taylor Schinvelski <taylorbyks@hotmail.com>
2020-08-10 21:48:45 -0300

    Hamburger Menu Update

--- src/components/HamburgerMenu/index.js
+++ src/components/HamburgerMenu/index.js
@@ -1,22 +1,37 @@
 import React from 'react'
 
 import './HamburgerMenu.css'
-
+import ButtonLink from '../ButtonLink'
 
 function HamburgerMenu(){
     return(
-        
-        
         <div className="HamburgerMenu">
-            <div className="Menu">
-            <h2>TESTET</h2>
+              <div class="menu-wrap">
+    <input type="checkbox" class="toggler" />
+    <div class="hamburger"><div></div></div>
+    <div class="menu">
+      <div>
+        <div>
+          <ul>
+            <li><ButtonLink className="HamMenu" to='/treinamentos'>
+                <h3>Treinamentos</h3>
+            </ButtonLink></li>
+            <li><ButtonLink className="HamMenu" to="/ondeestamos">
+                <h3>Onde Estamos</h3>
+            </ButtonLink></li>
+            <li><ButtonLink className="HamMenu" to="/serviços">
+                <h3>Serviços</h3>
+            </ButtonLink></li>
+            <li><ButtonLink className="HamMenu" to="/contato">
+                <h3>Contato</h3>
+            </ButtonLink></li>
+          </ul>
         </div>
-            <input type="checkbox" id="checkbox-menu" />
-            <label for="checkbox-menu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
+    </div>
+    </div>
+    </div>
+   
+ 
         </div>
     )
 }
